874c5fde0f752011ba248a4ade522c6696f06192
Garrett McQuigg <garrettmcquigg@gmail.com>
2023-06-01 18:16:04 -0500

    added config files

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -4,12 +4,25 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 
 	"server/internal"
 )
 
 func main() {
 	router := gin.Default()
+
+	v := viper.New()
+	v.SetConfigFile("config.yaml")
+
+	if err := v.ReadInConfig(); err != nil {
+		fmt.Println("Error:", err)
+        return
+	}
+
+	serverURL := v.GetString("server.host")
+	serverPort := v.GetString("server.port")
+	fmt.Println("host server URL:", serverURL + ":" + serverPort)
 
 	err := internal.SendEmail()
 	if err != nil {
